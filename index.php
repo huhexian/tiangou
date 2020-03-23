ddb97a880c075c4093c7117a5517ce388caf6885
青山 <42113492+hhx546642451@users.noreply.github.com>
2020-03-23 19:11:24 +0800

    舔狗日记
    TianGou1.0

---
+++ index.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> 舔狗日记</title>
+		<meta name="keywords" content="舔狗日记,青山绿水,www.huhexian.com">
+        <meta name="description" content="这是一个舔狗日记集合网站">
+        <link rel="shortcut icon" href="favicon.ico">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="./static/css/main.css" />
+		<noscript><link rel="stylesheet" href="./static/css/noscript.css" /></noscript>
+	</head>
+	<body onselectstart="return false" oncontextmenu=self.event.returnValue=false> 
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span>舔狗日记</span>						</div>
+						<div class="content">
+							<div class="inner">
+								<h2><?php echo date("Y年m月d日D")?>   晴</h2>
+								<h3 id="sentence"></h3>
+							</div>
+						</div>
+                   <button class="btn btn-primary btn-filled btn-xs" onclick="randomSentence()">再来一段</button>
+						<nav>
+					</header>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">©青山 版权所有<a target="_black" href='https://www.huhexian.com'>青山发布</a></p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="./static/js/jquery.min.js"></script>
+			<script src="./static/js/skel.min.js"></script>
+			<script src="./static/js/util.js"></script>
+			<script src="./static/js/main.js"></script>
+            <script src="./static/js/index.js"></script>
+
+ 
+
+	</body>
+</html>
